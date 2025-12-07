bcf282a3d7418ee32cafd9e922cdd6815c063b52
Rhafaely Ananda Costa dos Reis <rhafaelyreis@gmail.com>
2025-12-07 18:14:22 -0400

    cria ClienteRequestDTO e ClienteResponseDTO

---
+++ cinema/src/main/java/br/edu/ifms/cinema/dto/ClienteResponseDTO.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifms.cinema.dto;
+
+/**
+ *
+ * @author rhafa
+ */
+public class ClienteResponseDTO {
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private boolean status;
+    private String message;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getTelefone() { return telefone; }
+    public void setTelefone(String telefone) { this.telefone = telefone; }
+
+    public boolean isStatus() { return status; }
+    public void setStatus(boolean status) { this.status = status; }
+
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
+}
